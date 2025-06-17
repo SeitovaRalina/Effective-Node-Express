3120e50b312e1084757f3802c41c391c9531d080
Ralina <seitovaralina1@gmail.com>
2025-06-17 20:43:45 +0600

    feature: connect to MongoDB

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoURI = "";
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoURI);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
